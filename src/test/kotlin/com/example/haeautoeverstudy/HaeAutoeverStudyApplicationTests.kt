9cd2ef58ceed35152d86b1efa5f440504b7f923d
Yongha Kwon <yongha.kwon12@gmail.com>
2026-05-24 15:01:28 +0900

    init: first commit

---
+++ src/test/kotlin/com/example/haeautoeverstudy/HaeAutoeverStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.haeautoeverstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HaeAutoeverStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
